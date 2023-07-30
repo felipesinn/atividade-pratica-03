04c552c91f566759da20956e35c7aae380e1b132
felipesinn <felipesinn01@gmail.com>
2023-07-30 12:32:55 -0300

    inicio atividaes 3

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+let userN =Number(prompt("Digite o multiplicador"));
+let userI =Number(prompt("Digite quanjras X deseja Multiplicar"));
+
+
+
+for (let i = 1; i<=  userN; i++){
+   document.write(`${userN} * ${i} = ${userN * i} </p> `)
+}
+
+// Escreva um algoritmo que faça a tabuada de um número e
+// apresente o resultado de acordo com o modelo a seguir, sabendo
+// que o multiplicando deverá ser fornecido pelo usuário (n) e a
+// quantidade de iterações também (i).
+// 1 * 2 = 2
+// 2 * 2 = 4
+// 3 * 2 = 6
+// ...
